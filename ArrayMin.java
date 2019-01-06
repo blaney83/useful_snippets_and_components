4c519036922a5101b30ced8120ea25b789bc6536
Blaney83 <professionallaney@gmail.com>
2019-01-05 23:42:53 -0700

    Added array reverse and min snippets.

---
+++ ArrayMin.java
@@ -0,0 +1,36 @@
+package com.blaney83;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter count: ");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+	    int[] myArray = buildArray(count);
+	    findMin(myArray);
+    }
+
+    public static int[] buildArray(int number){
+        System.out.println("Enter " + number + " integers to start: ");
+        int[] builtArr = new int[number];
+        for(int i = 0; i < number; i ++){
+            builtArr[i] = scanner.nextInt();
+        }
+        return builtArr;
+    }
+
+    public static void findMin (int[] array){
+        System.out.println("Your numbers " + Arrays.toString(array));;
+        int minVal = Integer.MAX_VALUE;
+        for(int element: array){
+            if(element < minVal){
+                minVal = element;
+            }
+        }
+        System.out.println("The min val is: " + minVal);
+    }
+}
---
+++ ReverseArray.java
@@ -0,0 +1,34 @@
+package com.blaney83;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter count: ");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+        int[] myArray = buildArray(count);
+        reverseArray(myArray);
+    }
+    public static int[] buildArray(int number){
+        System.out.println("Enter " + number + " integers to start: ");
+        int[] builtArr = new int[number];
+        for(int i = 0; i < number; i ++){
+            builtArr[i] = scanner.nextInt();
+        }
+        return builtArr;
+    }
+
+    public static void reverseArray (int[] array){
+        int[] reversedArray = new int[array.length];
+        int revInd = 0;
+        for(int i = array.length ; i > 0; i --){
+            reversedArray[revInd] = array[i-1];
+            revInd ++;
+        }
+        System.out.println(Arrays.toString(reversedArray));
+    }
+}
