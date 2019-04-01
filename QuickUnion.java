83bc82684b40ff2ecd2677e8066f8c2ca7b18a76
Blaney83 <professionallaney@gmail.com>
2019-03-31 17:52:56 -0700

    Psuedo Quick Union hack, id and sz array implementation Quick Union added.

--- QuickUnion.java
+++ QuickUnion.java
@@ -1,20 +1,22 @@
-//array represents a set of trees "forrest"
+//array represents a set of trees "forest"
 //value x for index i points to its parent in the tree
 //if (x == i) => i is the root element
 //root i == id[id[..id[i]..]]
 public class QuickUnion {
 
     private int[] id;
-
+    private int[] sz;
+    //O(N)
     public QuickUnion(final int N) {
 
         id = new int[N];
 
         for (int i = 0; i < N; i++) {
             id[i] = i;
+            sz[i] = 1;
         }
     }
-
+    //O(N)
     private int findRoot(final int val) {
         int parent = val;
         while (parent != id[parent]) {
@@ -26,11 +28,21 @@ public class QuickUnion {
     public boolean isConnected(int valOne, int valTwo){
         return findRoot(valOne) == findRoot(valTwo);
     }
-
+    //connects smaller tree to root of bigger tree to prevent
+    //non-performant deep, skinny trees
     public void union(final int valOne, final int valTwo) {
         int i = findRoot(valOne);
         int j = findRoot(valTwo);
+        if(i == j){
+            return;
+        }
+        if(sz[i] < sz[j]){
+            id[i] = j;
+            sz[j] += sz[i];
+        }else{
+            id[j] = i;
+            sz[i] += sz[j];
+        }
         id[i] = j;
     }
-
 }
---
+++ QuickUnionPseudo.java
@@ -0,0 +1,57 @@
+//array represents a set of trees "forest"
+//value x for index i points to its parent in the tree
+//if (x == i) => i is the root element
+//root i == id[id[..id[i]..]]
+public class QuickUnionPseudo {
+
+    private int[] id;
+    //O(N)
+    public QuickUnionPseudo(final int N) {
+
+        id = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+    //O(N)
+    private int findRoot(final int val) {
+        int parent = val;
+        while (parent != id[parent]) {
+            parent = id[parent];
+        }
+        return parent;
+    }
+    //weighted implementation
+    private int[] smartRoot(final int val){
+        int parent = val;
+        int iter = 0;
+        while (parent != id[parent]) {
+            parent = id[parent];
+            iter ++;
+        }
+        return new int[]{parent, iter};
+    }
+
+    public boolean isConnected(int valOne, int valTwo){
+        return findRoot(valOne) == findRoot(valTwo);
+    }
+    //O(N), if passing roots, isRoot = true
+    public void union(final int valOne, final int valTwo, final boolean isRoots) {
+        int i = findRoot(valOne);
+        int j = findRoot(valTwo);
+        id[i] = j;
+    }
+    //joins small tree to root of larger tree to prevent tall, skinny, 
+    //non-performant trees
+    public void weightedUnion(final int valOne, final int valTwo){
+        int[] smRt1 = smartRoot(valOne);
+        int[] smRt2 = smartRoot(valTwo);
+        if(smRt2[1] < smRt1[1]){
+            union(smRt1[0], smRt2[0], true);
+        }else{
+            union(smRt2[0], smRt1[0], true);
+        }
+    }
+
+}
